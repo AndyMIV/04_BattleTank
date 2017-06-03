c3ff4370d5f4c628f58f19baea28feb7acb3f925
AndyMIV <amontoyaiv@gmail.com>
2017-06-02 22:33:47 -0500

    Skeleton for aiming at crosshair created

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -18,9 +18,38 @@ void ATankPlayerController::BeginPlay() {
 	UE_LOG(LogTemp, Warning, TEXT("Player Controller Begin Play"));
 }
 
+void ATankPlayerController::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+	PrimaryActorTick.bCanEverTick = true;
+	AimTowardsCrosshair();
+}
+
+
+
 ATank* ATankPlayerController::GetControlledTank() const {
 	// returns the pawn that the player controller is currently posessing. works because it is based off of player controller.
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardsCrosshair() {
+	if (!GetControlledTank()) { return; }
 
+	FVector HitLocation; // out parameter
+
+	bool HitCheck = GetSightRayHitLocation(HitLocation);
+	if (!HitCheck) {
+		UE_LOG(LogTemp, Warning, TEXT("Did not hit: %s"), *HitLocation.ToString());
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Direct Hit: %s"), *HitLocation.ToString());
+	}
+	// Get world location of linetrace through crosshair
+	// if it hits landscape
+		// tell controlled tank to aim at this point
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector &OutHitLocation) const {
+	// if it hits a valid object, return true and change outparameter
+	// if it hots an invalid object, return false 
+	return false;
+}
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -15,6 +15,13 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	void BeginPlay() override;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 	ATank * GetControlledTank() const;
+
+
+private:
+	// starter the tank moving the barrel so that a shot would hit where the crosshair intersects
+	void AimTowardsCrosshair();
+	bool GetSightRayHitLocation(FVector &HitLocation) const;
 };
