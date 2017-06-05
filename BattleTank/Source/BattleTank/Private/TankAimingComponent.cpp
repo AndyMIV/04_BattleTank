19c08e3a313392865463b84613bdcd829e647508
AndyMIV <amontoyaiv@gmail.com>
2017-06-04 20:12:19 -0500

    Elevate turret functioning

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -73,5 +73,5 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection) const {
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 	// UE_LOG(LogTemp, Warning, TEXT("AimAsRotator: %s"), *AimAsRotator.ToString());
 
-	Barrel->Elevate(5); // TODO remove magic number
+	Barrel->Elevate(DeltaRotator.Pitch); // TODO remove magic number
 }
--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -6,9 +6,13 @@
 
 void UTankBarrel::Elevate(float RelativeSpeed) {
 	// move barrel the right ammount this frame
+	// given a max elevation speed, and the frame time. (scaling) - time dialation
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds; // deltatimeseconds = this frame. this makes sure its framerate independent
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
 
-
-	// given a max elevation speed, and the frame time. (scaling)
-
+	RawNewElevation = FMath::Clamp<float>(RawNewElevation, MinElevation, MaxElevation);
+	SetRelativeRotation(FRotator(RawNewElevation, 0, 0));
+	
 }
 
